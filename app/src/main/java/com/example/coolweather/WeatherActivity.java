c7e410b807d5ca9ddbf387968313a2a72897c248
dummersoul <dummersoul@github.com>
2022-02-22 11:04:54 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -0,0 +1,11 @@
+package com.example.coolweather;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class WeatherActivity {
+    public DrawerLayout drawerLayout;
+    public Object swipeRefreshLayout;
+
+    public void requestWeather(String weatherId) {
+    }
+}
