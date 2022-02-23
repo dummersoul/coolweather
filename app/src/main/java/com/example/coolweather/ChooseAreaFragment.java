0d80269d73cc516b4cdda395f9798d62042c92f9
dummersoul <dummersoul@github.com>
2022-02-23 09:41:55 +0800

    加入显示天气信息的功能.

--- app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -81,9 +82,12 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
-                }
-//                else if(currentLevel==LEVEL_COUNTY){
-//                    String weatherId = countyList.get(position).getWeatherId();
+                } else if(currentLevel==LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);    // 向intent传入WeatherId
+                    startActivity(intent);
+                    getActivity().finish();
 //                    if(getActivity() instanceof WeatherActivity){   // 判断碎片的位置
 //                        //该碎片在WeatherActivity中，只需要刷新该活动
 //                        WeatherActivity activity = (WeatherActivity)getActivity();
@@ -96,7 +100,7 @@ public class ChooseAreaFragment extends Fragment {
 //                        startActivity(intent);
 //                        getActivity().finish();
 //                    }
-//                }
+                }
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
